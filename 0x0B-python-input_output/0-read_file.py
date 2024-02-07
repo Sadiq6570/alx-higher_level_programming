7724e92ff1e012e5d97330fd8e1dc4bc80f988a2
Sadiq6570 <sadeqmuhtar67@gmail.com>
2024-02-07 02:57:34 +0000

    done,m

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module That defines the read_file function"""
+
+
+def read_file(filename=""):
+    """Reading  a text file and prints it to stdout
+    Args:
+    filename (str): Filename
+    """
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            print(line, end="")
