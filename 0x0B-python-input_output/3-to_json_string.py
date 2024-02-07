7190c3a73d9a12e770331c2ffdb0ac551dd8463b
Sadiq6570 <sadeqmuhtar67@gmail.com>
2024-02-07 03:17:08 +0000

    hode

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module  contains a function  returns the JSON
+repr of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON represent of an object
+    Args:
+        my_obj: object
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    return json.dumps(my_obj)
