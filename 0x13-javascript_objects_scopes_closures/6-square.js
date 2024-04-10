434fec228593b515a2225e12cc1e0a04b9720de8
Sadiq6570 <sadeqmuhtar67@gmail.com>
2024-04-10 00:05:07 +0000

    nice language

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const SquareA = require('./5-rectangle.js');
+const Square = require('./5-rectangle.js');
 module.exports = class Square extends SquareA {
   charPrint (c) {
     if (c === undefined) {
