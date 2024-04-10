b6fe820786a09e385a6dae1e5d71b1afc611873f
Sadiq6570 <sadeqmuhtar67@gmail.com>
2024-04-10 00:01:09 +0000

    java script sweet language

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,7 +1,30 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let A = '';
+    for (let i = 0; i < this.width; i++) {
+      A += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(A);
+    }
+  }
+
+  rotate () {
+    const aux = this.height;
+    this.height = this.width;
+    this.width = aux;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
