8682ba88eacad2f740e7436401a6a636e8f77f09
Sadiq6570 <sadeqmuhtar67@gmail.com>
2024-04-27 04:35:57 +0000

    234

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Script that fetching the  https://alx-intranet.hbtn.io/status."""
-import requests
+""" 
+Script that takes in a URL, send a request to URL, and dispaly body
+"""
 
 
 if __name__ == "__main__":
-    req = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as resp:
+            print(resp.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
