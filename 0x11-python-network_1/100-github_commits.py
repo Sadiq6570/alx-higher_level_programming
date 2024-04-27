91a8610c1e33c2f82ca7a262379640d7e9d4d9fb
Sadiq6570 <sadeqmuhtar67@gmail.com>
2024-04-27 04:38:43 +0000

    p networks

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 """
-Python script that takes 2 arguments in order to solve this challenge.
-
-Usage: ./100-github_commits.py <repository name> <repository owner>
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
 """
-from sys import argv
 import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
 
-
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    req = requests.get(url)
-    commits = req.json()
-
-    try:
-        for index in range(10):
-            print("{}: {}".format(
-                commits[index].get("sha"),
-                commits[index].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
