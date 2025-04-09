c5daa0cbc135ac0a853c4b39e5853c5b338dee47
Sadiq6570 <sadeeeqmuhtar67@gmail.com>
2025-04-09 13:43:51 +0100

    helo

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' empty class'''
+
+class Square:    
+    '''passing square empty class'''
+
+    def __init__(self, size):
+        self._size = size     
+    '''private self'''
