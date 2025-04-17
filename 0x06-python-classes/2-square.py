6873d5234cd498903c1e3cceaeefb61e83ccb545
Sadiq6570 <sadeeeqmuhtar67@gmail.com>
2025-04-17 14:47:10 +0100

    hopre

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
-        if size != isinstance(size, int):
-            raise TypeError
-        print("size must be integer ")
-        if size > 0:
-            raise ValueError
-        print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be integer ")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
